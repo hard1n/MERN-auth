05af5a18e3309cb38f193f4285a40164ade5b0fb
Hardin <hardisonpaulino@gmail.com>
2024-04-25 17:20:56 -0400

    chore: mongoDB conection added, using  mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 8000;
 
-// MIDDLEWARES
+// CONNECT DATABASE
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   // console.log("Hello from server!");
   res.send("Hello from server!");
