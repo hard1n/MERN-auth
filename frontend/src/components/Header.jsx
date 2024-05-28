dd4b858a84e1872901813de12468e25e85d04917
Hardin <hardisonpaulino@gmail.com>
2024-05-28 11:59:42 -0400

    Feat:  Account dropdown menu render when logged in

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Header = () => {
-  // console.log(isAuthenticated);
+  const [session, setSession] = useState(false);
+  let navigate = useNavigate();
+
+  /** Checking Session **/
+  useEffect(() => {
+    // Check if a token exists in localStorage
+    const token = localStorage.getItem("token");
+    if (token) {
+      setSession(true);
+    }
+  }, []);
+
+  console.log(session);
+
+  const logOut = () => {
+    axios
+      .get("http://localhost:8000/api/logout")
+      .then((res) => {
+        localStorage.removeItem("token");
+        toast.success("Log Out succesfully!");
+        setSession(false);
+        navigate("/");
+      })
+      .catch((err) => console.log("Ocurió este error ", err));
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-light">
@@ -23,31 +50,65 @@ const Header = () => {
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home{" "}
-                </Link>
-              </li>
+            {session ? (
+              <>
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/">
+                      Home{" "}
+                    </Link>
+                  </li>
+                </ul>
+                <div className="dropdown">
+                  <button
+                    type="button"
+                    className="btn btn-secondary dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Account
+                  </button>
+                  <ul className="dropdown-menu menu__custom">
+                    <li>
+                      <Link
+                        className="dropdown-item text-info nav-link"
+                        to="/user/profile"
+                      >
+                        Profile{" "}
+                      </Link>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li onClick={logOut}>
+                      <Link className="dropdown-item text-danger nav-" to="">
+                        Log out{" "}
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </>
+            ) : (
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home{" "}
+                  </Link>
+                </li>
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup">
-                  Sign Up{" "}
-                </Link>
-              </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signup">
+                    Sign Up{" "}
+                  </Link>
+                </li>
 
-              <li className="nav-item">
-                <Link className="nav-link" to="/signin">
-                  Log In{" "}
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="">
-                  Log out{" "}
-                </Link>
-              </li>
-            </ul>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signin">
+                    Log In{" "}
+                  </Link>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </nav>
